67e3a85c416ab821f93a7d44129ec8ca278354d4
Reebal Sami <reebal.sami@gmail.com>
2024-02-05 11:08:04 +0100

    added first tow steps

--- src/main/java/neuefisched/de/Main.java
+++ src/main/java/neuefisched/de/Main.java
@@ -2,6 +2,13 @@ package neuefisched.de;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal cat = new Animal("01-0001", "Bagheera", "Bengal", 2);
+        Animal dog = new Animal("01-0002", "shivie", "german shiver", 7);
+
+        System.out.println("cat age is: " + cat.age());
+        System.out.println("my cat is a: " + cat);
+        System.out.println("dog name is: " + dog.name());
+        System.out.println("my dog is a: " + dog);
+
     }
 }
